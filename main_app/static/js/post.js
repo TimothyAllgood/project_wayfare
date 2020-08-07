21240f275e9a2eaa1e90538be4cccca7bc76d530
Timothy Allgood <timothyallgood@gmail.com>
2020-08-07 12:30:59 -0500

    hopefully fixed bug

--- main_app/static/js/post.js
+++ main_app/static/js/post.js
@@ -4,26 +4,30 @@ const postPrevious = document.querySelector('.previous-post-btn');
 
 postsMenus[0].classList.add('active-post-list');
 
+if (postsMenus.length < 2) {
+	postNext.style.display = 'none';
+	postPrevious.style.display = 'none';
+}
+
 let k = 0;
-postPrevious.style.display = 'none'
+postPrevious.style.display = 'none';
 
-
-postNext.addEventListener('click', (e)=>{
-	postPrevious.style.display = 'initial'
-	postsMenus[k].classList.remove('active-post-list')
+postNext.addEventListener('click', (e) => {
+	postPrevious.style.display = 'initial';
+	postsMenus[k].classList.remove('active-post-list');
 	k++;
-	postsMenus[k].classList.add('active-post-list')
-	if(k == postsMenus.length-1){
-		e.target.style.display='none'
-	}	
-})
+	postsMenus[k].classList.add('active-post-list');
+	if (k == postsMenus.length - 1) {
+		e.target.style.display = 'none';
+	}
+});
 
-postPrevious.addEventListener('click', (e)=>{
-    postNext.style.display = 'initial'
-    postsMenus[k].classList.remove('active-post-list')
-    k--;
-    postsMenus[k].classList.add('active-post-list')
-    if(k == 0){
-        e.target.style.display='none'
-    }
-})
+postPrevious.addEventListener('click', (e) => {
+	postNext.style.display = 'initial';
+	postsMenus[k].classList.remove('active-post-list');
+	k--;
+	postsMenus[k].classList.add('active-post-list');
+	if (k == 0) {
+		e.target.style.display = 'none';
+	}
+});
