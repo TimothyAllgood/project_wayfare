6a0387daf006afde1c89cb17617c66934c87a215
Timothy Allgood <timothyallgood@gmail.com>
2020-08-07 19:30:29 -0500

    ready

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,4 +3,4 @@ from .models import City, Post, Profile
 # Register your models here.
 admin.site.register(City)
 admin.site.register(Post)
-admin.site.register(Profile)
+# admin.site.register(Profile)
